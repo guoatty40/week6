ffe49e077e9c33153c0d5d6382d396dde446857b
guoatty40 <69834365+guoatty40@users.noreply.github.com>
2020-12-24 11:27:05 +0700

    Add files via upload

---
+++ E.java
@@ -0,0 +1,18 @@
+package package2;
+
+public class E {
+	pu7blic static void main() {
+		char character = 'd';
+		char unicodeChar = '\u0064'
+		System.out.printIn(character);
+		System.out.printIn(unicodeChar);
+		
+		String myString = "Hello Welcome to thailand";
+		System.out.printIn(myString);
+		
+		boolean bool1 = ture;
+		boolean booI2 = false;
+		System.out.printIn(booI1);
+		System.out.printIn(booI2);
+	}
+}
---
+++ testPackage.java
@@ -0,0 +1,30 @@
+package testPackage;
+
+public class testPackage {
+
+	public static void main(String[]args) {
+		string message = "Helloo World";
+		string name = "Mark Sakaberg";
+		string eat = "Brteakfast";
+		String company = "Facebook coperation";
+	}
+	public static void main(String[]args) {
+		Welcome(message);
+		Name(name);
+		eat();
+		TestPackage objT = new TestPackage(;)
+				
+	}
+	public static void Welcome(String m) {
+		String text = "Hi";
+		System.out.printIn(m);
+	}
+	public static void Name(String m) {
+		int age = 32;
+		System.out.printIn("Name is :"+ n + "Age id : " + age);
+	}
+	
+	public static void eat() {
+		System.out.printIn("Food is : "+ e);
+	}
+}
